7d6a2e44e42bcadd234d147fa2b7d3e4b68360a0
lokeshmeher <lokeshmeher34@gmail.com>
2016-02-05 17:39:12 +0530

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+#include <iostream>
+#include <array>
+#include <cstdlib>	// for srand() and rand()
+#include <ctime>	// for time()
+#include "cards.h"
